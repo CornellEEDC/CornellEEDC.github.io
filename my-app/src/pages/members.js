8e7a18de9dd4bf3b30a745e048e5d357f9b6d325
Emily Penna <emilypenna99@gmail.com>
2023-11-29 18:09:15 -0500

    default members page added - needs tweaking

--- my-app/src/pages/members.js
+++ my-app/src/pages/members.js
@@ -1,13 +1,4 @@
-import jillian from "./JILLIANPARRINO.jpg"
-
-// Helper Functions
-function Header() {
-  return (
-    <title>
-    CEEDC Meet the Peeps
-    </title>
-  )
-}
+import basic from "./basic.png"
 
 function MemberIcon() {
   return (
@@ -17,7 +8,7 @@ function MemberIcon() {
         </h3>
         <aside>
             <div className="parent">
-                <img className= "child" src ={jillian} style={{width:"75%", border:"5px double rgb(255, 0, 0)"}} ></img>
+                <img className= "child" src ={basic} style={{width:"75%", border:"5px double rgb(255, 0, 0)"}} ></img>
                 <b>
                     <p className="child">President, Engineering Lead, Production Lead & Founding Member</p> 
                 </b> 
@@ -31,16 +22,99 @@ function MemberIcon() {
 function Members() {
   return (
       <div className="Main">
-        <p>pens</p>
+
+        
+        <div style={{backgroundColor: "red"}}>
+          <br></br>
+          <br></br>
+          <h1 className="Members-h1">
+              <b>
+                  Meet The Peeps
+                  
+              </b>
+          </h1>
+          <br></br>
+        </div>
+        <br></br>
+
+        <h2 className="Members-h2">
+            E-Board
+        </h2>
+        <h3>
+        Those who make the magic happen...
+        </h3>
+
+        <div className="row">
+          <MemberIcon />
+          <MemberIcon />
+          <MemberIcon />
+          <MemberIcon />
+          <MemberIcon />
+        </div>
+        <br></br>
+
+        <h2 className="Members-h2">
+            Engineering
+        </h2>
+
+        <h3>
+        Mechanical
+        </h3>
+
+        <div className="row">
+          <MemberIcon />
+          <MemberIcon />
+          <MemberIcon />
+          <MemberIcon />
+          <MemberIcon />
+        </div>
+        <br></br>
+
+        <h3>
+        Software
+        </h3>
+
+        <div className="row">
+          <MemberIcon />
+          <MemberIcon />
+          <MemberIcon />
+          <MemberIcon />
+          <MemberIcon />
+        </div>
+        <br></br>
+
+        <h2 className="Members-h2">
+            Production
+        </h2>
+
+        <h3>
+        Our lovely Production team
+        </h3>
+
         <div className="row">
           <MemberIcon />
           <MemberIcon />
         </div>
+        <br></br>
+
+        <h2 className="Members-h2">
+            Business
+        </h2>
+
+        <h3>
+        The business team is responsible for organising our Events and communicating with sponsors...
+        </h3>
+
         <div className="row">
           <MemberIcon />
           <MemberIcon />
         </div>
+        <br></br>
+
+
       </div>
+
+
   );
 }
 
