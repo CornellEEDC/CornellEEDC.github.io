b9052dd5a2f6f476708e1c45badebb501799ab93
Emily Penna <emilypenna99@gmail.com>
2023-11-29 14:50:42 -0500

    import homepage in react

---
+++ my-app/src/components/navbar.js
@@ -0,0 +1,57 @@
+// 'use strict';
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+// import '../styles/main.css';
+
+
+// function Nav() {
+//   return (
+//     <div>
+//     <nav id = "nav"> 
+//         <ul>
+//             <li><a class = "active" href='index.html'>Home</a></li>
+//             <li><a href='members.js'>Meet our peeps</a></li>
+//             <li><a href='projects.html'>Projects</a></li>
+//         </ul>
+//     </nav>
+//     </div>
+//   )
+// }
+
+function ColorSchemesExample() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default ColorSchemesExample
+// export default Nav
+
+
+
---
+++ my-app/src/pages/Home.js
@@ -0,0 +1,23 @@
+import '../styles/main.css';
+
+
+function Home() {
+  return (
+    <div style={{marginTop:'60px', marginLeft:'20px'}} >
+        <h2> Cornell Entertainment Engineering & Design Club</h2>
+        <h4> Welcome to CEEDC!</h4>
+        <p> <b>Cornell Entertainment Engineering & Design Club</b> 
+            is Cornell's premier club devoted to the newest technology in entertainment</p>
+        <b>Our Mission</b>
+        <p>The mission of <b>CEEDC</b> is to inspire the next generation of entertainment innovators through our three pillars.</p>
+        <b>CEEDC's Three Pillars</b>
+        <ul>
+            <li> Industry Education </li>
+            <li> Community Projects</li>
+            <li> Interdisiplinary Collaboration</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Home;
