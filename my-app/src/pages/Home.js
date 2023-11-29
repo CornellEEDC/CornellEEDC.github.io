ef6da2aa3ecbb1cff0ee1a0721ca881bbc7ac7bb
Emily Penna <emilypenna99@gmail.com>
2023-11-29 18:28:34 -0500

    carousel component to home page

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -1,5 +1,39 @@
 // import '../styles/main.css';
 
+import Carousel from 'react-bootstrap/Carousel';
+// import ExampleCarouselImage from 'components/ExampleCarouselImage';
+
+function IndividualIntervalsExample() {
+  return ( 
+    <div style={{ display: 'block', width: 700, padding: 30 }}> 
+      <Carousel> 
+        <Carousel.Item interval={1500}> 
+          <img 
+            className="d-block w-100"
+src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122739/2-300x115.png"
+            alt="Image One"
+          /> 
+          <Carousel.Caption> 
+            <h3>Label for first slide</h3> 
+            <p>Sample Text for Image One</p> 
+          </Carousel.Caption> 
+        </Carousel.Item> 
+        <Carousel.Item interval={500}> 
+          <img 
+            className="d-block w-100"
+src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122716/1-300x115.png"
+            alt="Image Two"
+          /> 
+          <Carousel.Caption> 
+            <h3>Label for second slide</h3> 
+            <p>Sample Text for Image Two</p> 
+          </Carousel.Caption> 
+        </Carousel.Item> 
+      </Carousel> 
+    </div> 
+  ); 
+}
+
 function Home() {
   return (
     <div className="Main" >
@@ -15,6 +49,10 @@ function Home() {
             <li> Community Projects</li>
             <li> Interdisiplinary Collaboration</li>
         </ul>
+
+        <IndividualIntervalsExample/>
+
+
     </div>
   )
 }
